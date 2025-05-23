7bd2241754333cc8a501861549f7a7cf22145ec8
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-05-24 03:46:17 +0530

    worked on the cor

--- api/data.js
+++ api/data.js
@@ -1,6 +1,14 @@
 let dataStore = [];
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method === 'POST') {
     const { id, name, currentTime } = req.body;
 
