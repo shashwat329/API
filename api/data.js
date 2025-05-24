4e263be6d66de5a4af51ecd5084f94bde96a04b7
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-05-24 13:51:15 +0530

    update the path

--- api/data.js
+++ api/data.js
@@ -1,5 +1,5 @@
-import { connectToDatabase } from '../../lib/mongodb';
-import Entry from '../../models/Entry';
+import { connectToDatabase } from '../lib/mongodb';
+import Entry from '../models/Entry';
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
